7487ec323dea01a9c82b02f6ebff148df60be1bc
Matrix <immatrixchan@gmail.com>
2013-12-07 11:39:51 +0800

    before  use generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext,loader
 from django.http import Http404
+from django.core.urlresolvers import reverse
 
-from polls.models import Poll
+from polls.models import Poll,Choice
 
 
 # Create your views here.
@@ -29,7 +30,21 @@ def detail(request,poll_id):
 
 
 def results(request,poll_id):
-    return HttpResponse("You're looking at the results")
+    poll = get_object_or_404(Poll,pk=poll_id)
+    return render(request,'polls/results.html',{'poll':poll})
 
 def vote(request,poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll,pk = poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'poll':p,
+            'error_message':"You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args = (p.id,)))
+    #return HttpResponse("You're voting on poll %s." % poll_id)
+
